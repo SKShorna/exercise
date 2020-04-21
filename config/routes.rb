4edd523f061e7f1a70cd3d0c71ec2de972e53df8
Sabira Shorna <shorna.sabira@gmail.com>
2020-04-21 03:17:06 -0400

    Complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   
-  root to: redirect('/welcome', status: 302)
+  #root to: redirect('/welcome', status: 302)
+  root to: redirect('/profile', status: 302)
   get 'profile', to: 'pages#profile', as: 'profile'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
